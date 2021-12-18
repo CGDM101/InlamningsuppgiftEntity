468e0edb0ba7c72220e55d91e34c2526b59ea0bf
CGDM101 <51097129+CGDM101@users.noreply.github.com>
2021-12-18 15:54:26 +0100

    List all persons in database

--- InlamningsuppgiftEntity/Program.cs
+++ InlamningsuppgiftEntity/Program.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore; // För att kunna använda DbContext
 using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using System.Linq;
 
 namespace InlamningsuppgiftEntity
 {
@@ -24,7 +26,8 @@ namespace InlamningsuppgiftEntity
         {
             modelBuilder.Entity<Person>().HasData(new Person { Id = 1, Name = "Dave", LastName = "Gahan", Mor = 2, Far = 3 }, new Person { Id = 2, Name = "Sylvia", LastName = "Gahan", Mor = 0, Far = 0 }, new Person { Id = 3, Name = "Len", LastName = "Gahan", Mor = 0, Far = 0 }, new Person { Id = 4, Name = "Sue", LastName = "Gahan", Mor = 2, Far = 3 }, new Person { Id = 5, Name = "Peter", LastName = "Gahan", Mor = 2, Far = 7 }, new Person { Id = 6, Name = "Phil", LastName = "Gahan", Mor = 2, Far = 7 }, new Person { Id = 7, Name = "Jack", LastName = "Gahan", Mor = 0, Far = 0 }, new Person { Id = 8, Name = "Jack", LastName = "Gahan", Mor = 9, Far = 1 }, new Person { Id = 9, Name = "Joanne", LastName = "Fox", Mor = 0, Far = 0 }, new Person { Id = 10, Name = "Stella Rose", LastName = "Gahan", Mor = 11, Far = 1 }, new Person { Id = 11, Name = "Jennifer", LastName = "Sklias-Gahan", Mor = 12, Far = 0 }, new Person { Id = 12, Name = "Stella", LastName = "Sklias", Mor = 0, Far = 0 }, new Person { Id = 13, Name = "Jimmy", LastName = "Rogers-Gahan", Mor = 11, Far = 0 });
 
-
+            List<Person> listOfAllPersons = new List<Person>();
+            // lägg till dem
 
             // modelBuilder.Seed();
         }
@@ -77,6 +80,20 @@ namespace InlamningsuppgiftEntity
 
             Console.WriteLine("Hello World!");
             Console.WriteLine("Nu skapar vi en databas!");
+
+
+            Console.WriteLine("Lista alla personer (förnamn):");
+            using (var context = new GenealogiContext())
+            {
+                var allPeopleInDatabase = context.MyPeople.ToList();
+                Console.WriteLine("varsågo:");
+                foreach (var item in allPeopleInDatabase)
+                {
+                    Console.WriteLine(item.Name);
+                }
+            }
+
+
 
             // Create person:
             // r
